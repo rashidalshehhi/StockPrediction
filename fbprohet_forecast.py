a8d42059b644bf4f1b50340857497fa576c91657
rashidalshehhi <74825106+rashidalshehhi@users.noreply.github.com>
2023-03-09 23:16:52 +0400

    Update fbprohet_forecast.py

--- fbprohet_forecast.py
+++ fbprohet_forecast.py
@@ -1,53 +1,65 @@
+# pip install streamlit fbprophet yfinance plotly
 import streamlit as st
-from fbprophet import Prophet
-from datetime import datetime, timedelta
+from datetime import date
+
 import yfinance as yf
+from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
 
-# function to load data and perform predictions
-def load_data(ticker, year):
-    # load data from yahoo finance
-    stock_data = yf.download(ticker, start="2015-01-01", end=datetime.now())
-    stock_data = stock_data.reset_index()
-    stock_data = stock_data[["Date", "Close"]]
-    stock_data = stock_data.rename(columns={"Date": "ds", "Close": "y"})
+st.title('Stock Forecast App')
 
-    # create Prophet model
-    model = Prophet()
-    model.fit(stock_data)
+stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
+selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
-    # create future dataframe for predictions
-    future = model.make_future_dataframe(periods=365)
-    future = future[future["ds"].dt.year == year]
-
-    # perform predictions
-    forecast = model.predict(future)
-
-    # plot predictions
-    fig = plot_plotly(model, forecast)
-    fig.update_layout(title=f"Predictions for {ticker} in {year}", xaxis_title="Date", yaxis_title="Close Price")
-    fig.update_yaxes(fixedrange=True)
-
-    return forecast, fig
+n_years = st.slider('Years of prediction:', 1, 4)
+period = n_years * 365
 
 
-# create web app interface
-st.title("Stock Prediction Web App")
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
 
-# get user input for stock symbol and year
-ticker = st.sidebar.text_input("Enter stock symbol (e.g. AAPL for Apple):")
-year = st.sidebar.slider("Enter year to predict for:", min_value=2015, max_value=datetime.now().year, step=1)
+	
+data_load_state = st.text('Loading data...')
+data = load_data(selected_stock)
+data_load_state.text('Loading data... done!')
 
-# load data and perform predictions
-if ticker:
-    forecast, fig = load_data(ticker, year)
+st.subheader('Raw data')
+st.write(data.tail())
 
-    # display predictions
-    st.plotly_chart(fig)
+# Plot raw data
+def plot_raw_data():
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+	st.plotly_chart(fig)
+	
+plot_raw_data()
 
-    st.write(f"Predictions for {ticker} in {year}:")
-    st.write(forecast[["ds", "yhat"]].tail())
+# Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
 
+# Show and plot forecast
+st.subheader('Forecast data')
+st.write(forecast.tail())
+    
+st.write(f'Forecast plot for {n_years} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write("Forecast components")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
